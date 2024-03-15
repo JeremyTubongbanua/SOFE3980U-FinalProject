2c3e7c1b8b0cdb706fbe3f0777cd6e3050163e04
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-03-15 17:37:40 -0400

    feat: changeTimeFormat

--- backend/TimeUtils.js
+++ backend/TimeUtils.js
@@ -13,4 +13,27 @@ const calculateAirTime = (time1, timezoneoffset1, time2, timezoneoffset2) => {
     return Math.abs(time1InUTC - time2InUTC);
 }
 
-module.exports = { calculateAirTime }
+/**
+ * Change time format from 24 hour to 12 hour or vice versa
+ * @param {*} time example: 1200
+ * @param {*} mode example: '12' or '24'
+ * @returns example: 1400 -> 200
+ */
+const changeTimeFormat = (time, mode) => {
+    if (mode === '12') {
+        if (time < 1200) {
+            return time + 1200;
+        } else if (time >= 1300) {
+            return time - 1200;
+        }
+    } else if (mode === '24') {
+        if (time < 100) {
+            return time + 1200;
+        } else if (time >= 100 && time < 800) {
+            return time + 2400;
+        }
+    }
+    return time;
+}
+
+module.exports = { calculateAirTime, changeTimeFormat }
