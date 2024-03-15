b4bc0584cedb3838a52cdeabbb857a47f090f990
JerryShum <jerryshum2636@gmail.com>
2024-03-15 14:50:11 -0400

    added jest --coverage

--- backend/tests/airtime.test.js
+++ backend/tests/airtime.test.js
@@ -1,12 +1,12 @@
-const {newFlight, calculateAirTime} = require('../Flight');
-const {getOffset} = require('../Airport');
+const { newFlight, calculateAirTime } = require('../Flight');
+const { getOffset } = require('../Airport');
 
 // Generate Flights
 const f1 = newFlight(1, "Boeing 747", "2020-01-01", 1200, "2020-01-01", 1800, "YYZ", "YYC");
 const f2 = newFlight(2, "Boeing 747", "2020-01-01", 1500, "2020-01-01", 2200, "YYC", "YVR");
 
 // Test calculateAirTime
-test('calculateAirTime', () => {
-    expect(calculateAirTime(f1.departtime, getOffset(f1.sourceid), f1.arrivetime, getOffset(f1.destinationid))).toBe(400);
-    expect(calculateAirTime(f2.departtime, getOffset(f2.sourceid), f2.arrivetime, getOffset(f2.destinationid))).toBe(600);
+test('properly calculates correct airtime', () => {
+    expect(calculateAirTime(f1.departtime, getOffset(f1.sourceid), f1.arrivetime, getOffset(f1.destinationid))).toEqual(400);
+    expect(calculateAirTime(f2.departtime, getOffset(f2.sourceid), f2.arrivetime, getOffset(f2.destinationid))).toEqual(600);
 });
