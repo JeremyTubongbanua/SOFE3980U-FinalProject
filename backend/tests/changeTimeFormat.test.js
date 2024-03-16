d413094f72d22b9c5c358efc3921ecdd7ea8a5df
JerryShum <jerryshum2636@gmail.com>
2024-03-15 22:02:37 -0400

    feat: added tests

--- backend/tests/changeTimeFormat.test.js
+++ backend/tests/changeTimeFormat.test.js
@@ -35,6 +35,12 @@ describe('changeTimeFormat', () => {
         // Test case: 23:00 → 11:00 pm
         expect(changeTimeFormat('23:00', '12')).toBe('11:00 pm');
 
+        // Test case: 12:30 -> 12:30 pm
+        expect(changeTimeFormat('12:30', '12')).toBe('12:30 pm');
+
+        // Test case: 9:00 -> 9:00 am
+        expect(changeTimeFormat('9:00', '12')).toBe('9:00 am');
+
         // Test case: 1:05 pm → 13:00
         expect(changeTimeFormat('1:05 pm', '24')).toBe('13:05');
 
@@ -43,10 +49,6 @@ describe('changeTimeFormat', () => {
 
         // Test case: 12:30 am → 00:30
         expect(changeTimeFormat('12:30 pm', '24')).toBe('12:30');
-
-        expect(changeTimeFormat('12:30', '12')).toBe('12:30 pm');
-
-        expect(changeTimeFormat('9:00', '12')).toBe('9:00 am');
     });
 
     it('should handle invalid input', () => {
@@ -59,8 +61,10 @@ describe('changeTimeFormat', () => {
         //Invalid Mode - not 12 or 24
         expect(() => changeTimeFormat('00:00', "123")).toThrow(IncorrectArgumentsError);
 
+        //Invalid Input - converting from 12 hour format to 12 hour format
         expect(() => changeTimeFormat('10:00 pm', "12")).toThrow(IncorrectArgumentsError);
 
+        //Invalid Input - converting from 24 hour format to 24 hour format
         expect(() => changeTimeFormat('12:00', "24")).toThrow(IncorrectArgumentsError);
 
 
