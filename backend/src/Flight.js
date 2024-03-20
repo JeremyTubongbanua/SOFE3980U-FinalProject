52c3d299e93066b7b85c8918b83167649bbb946d
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-03-20 17:07:06 -0400

    chore: more flights

--- backend/src/Flight.js
+++ backend/src/Flight.js
@@ -12,6 +12,8 @@ const newFlight = (flightid, planename, departdate, departtime, arrivedate, arri
     };
 }
 
+let i = 0;
+
 flights = [
     newFlight(1, "Boeing 747", "2020-01-01", 500, "2020-01-01", 1400, "YYZ", "YYC"),
     newFlight(2, "Boeing 747", "2020-01-01", 1500, "2020-01-01", 1600, "YYC", "YVR"),
@@ -30,6 +32,99 @@ flights = [
     newFlight(12, "Boeing 132", "2020-05-28", 1200, "2020-05-29", 300, "YYZ", "YYC"),
     newFlight(13, "Boeing 132", "2020-05-29", 1200, "2020-05-29", 2200, "YYC", "YVR"),
     newFlight(14, "AC-130", "2020-05-30", 0, "2020-05-30", 1500, "YVR", "SFO"),
+
+    // depart Mar 1, arrive Mar 1
+    newFlight(i++, "Boeing 123", "2020-03-01", 0, "2020-03-01", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-01", 1100, "2020-03-01", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-01", 100, "2020-03-01", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-01", 1200, "2020-03-01", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-01", 1500, "2020-03-01", 1600, "YYC", "YVR"),
+    newFlight(i++, "AC-130", "2020-03-01", 0, "2020-03-01", 1500, "YYC", "YVR"),
+
+    // depart Mar 2, arrive Mar 2
+    newFlight(i++, "Boeing 747", "2020-03-02", 0, "2020-03-02", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-02", 1100, "2020-03-02", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-02", 100, "2020-03-02", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-02", 1200, "2020-03-02", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-02", 1500, "2020-03-02", 1600, "YYC", "YVR"),
+    newFlight(i++, "AC-130", "2020-03-02", 0, "2020-03-02", 1500, "YYC", "YVR"),
+
+    // depart Mar 3, arrive Mar 3
+    newFlight(i++, "Boeing 747", "2020-03-03", 0, "2020-03-03", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-03", 1100, "2020-03-03", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-03", 100, "2020-03-03", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-03", 1200, "2020-03-03", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-03", 1500, "2020-03-03", 1600, "YYC", "YVR"),
+
+    // depart Mar 4, arrive Mar 4
+    newFlight(i++, "Boeing 747", "2020-03-04", 0, "2020-03-04", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-04", 1100, "2020-03-04", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-04", 100, "2020-03-04", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-04", 1200, "2020-03-04", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-04", 1500, "2020-03-04", 1600, "YYC", "YVR"),
+
+    // depart Mar 5, arrive Mar 5
+    newFlight(i++, "Boeing 747", "2020-03-05", 0, "2020-03-05", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-05", 1100, "2020-03-05", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-05", 100, "2020-03-05", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-05", 1200, "2020-03-05", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-05", 1500, "2020-03-05", 1600, "YYC", "YVR"),
+
+    // depart Mar 6, arrive Mar 6
+    newFlight(i++, "Boeing 747", "2020-03-06", 0, "2020-03-06", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-06", 1100, "2020-03-06", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-06", 100, "2020-03-06", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-06", 1200, "2020-03-06", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-06", 1500, "2020-03-06", 1600, "YYC", "YVR"),
+
+    // depart Mar 7, arrive Mar 7
+    newFlight(i++, "Boeing 747", "2020-03-07", 0, "2020-03-07", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-07", 1100, "2020-03-07", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-07", 100, "2020-03-07", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-07", 1200, "2020-03-07", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-07", 1500, "2020-03-07", 1600, "YYC", "YVR"),
+
+    // depart Mar 8, arrive Mar 8
+    newFlight(i++, "Boeing 747", "2020-03-08", 0, "2020-03-08", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-08", 1100, "2020-03-08", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-08", 100, "2020-03-08", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-08", 1200, "2020-03-08", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-08", 1500, "2020-03-08", 1600, "YYC", "YVR"),
+
+    // depart Mar 9, arrive Mar 9
+    newFlight(i++, "Boeing 747", "2020-03-09", 0, "2020-03-09", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-09", 1100, "2020-03-09", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-09", 100, "2020-03-09", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-09", 1200, "2020-03-09", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-09", 1500, "2020-03-09", 1600, "YYC", "YVR"),
+
+    // depart Mar 10, arrive Mar 10
+    newFlight(i++, "Boeing 747", "2020-03-10", 0, "2020-03-10", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-10", 1100, "2020-03-10", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-10", 100, "2020-03-10", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-10", 1200, "2020-03-10", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-10", 1500, "2020-03-10", 1600, "YYC", "YVR"),
+
+    // depart Mar 11, arrive Mar 11
+    newFlight(i++, "Boeing 747", "2020-03-11", 0, "2020-03-11", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-11", 1100, "2020-03-11", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-11", 100, "2020-03-11", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-11", 1200, "2020-03-11", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-11", 1500, "2020-03-11", 1600, "YYC", "YVR"),
+
+    // depart Mar 12, arrive Mar 12
+    newFlight(i++, "Boeing 747", "2020-03-12", 0, "2020-03-12", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-12", 1100, "2020-03-12", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-12", 100, "2020-03-12", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-12", 1200, "2020-03-12", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-12", 1500, "2020-03-12", 1600, "YYC", "YVR"),
+
+    // depart Mar 13, arrive Mar 13
+    newFlight(i++, "Boeing 747", "2020-03-13", 0, "2020-03-13", 1000, "YYZ", "YYC"),
+    newFlight(i++, "Boeing 747", "2020-03-13", 1100, "2020-03-13", 1800, "YYC", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-13", 100, "2020-03-13", 1300, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-13", 1200, "2020-03-13", 1400, "YYZ", "YVR"),
+    newFlight(i++, "Boeing 747", "2020-03-13", 1500, "2020-03-13", 1600, "YYC", "YVR")
 ];
 
 const getFlight = (flightid) => {
