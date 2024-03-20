03f9fbc978e8d83237b8974f2558e03477ad7cbb
Jeremy Tubongbanua <jeremy.tubongbanua@gmail.com>
2024-03-20 19:11:48 -0400

    test: calculateAirTimeFlights

--- backend/tests/calculateAirTimeFlights.test.js
+++ backend/tests/calculateAirTimeFlights.test.js
@@ -1,6 +1,12 @@
-const { calculateAirTimeFlights } = require('../src/Flight');
+const { calculateAirTimeFlights, getFlight } = require('../src/Flight');
 // TODO
 
 test('dummy', () => {
-
+    const flights = [
+        getFlight(1),
+        getFlight(2)
+    ];
+    const result = calculateAirTimeFlights(flights);
+    const expected = 700;
+    expect(result).toBe(expected);
 });
