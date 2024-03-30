405db66df0094b725fae24af5c71c29e5d4eda8c
JerryShum <jerryshum2636@gmail.com>
2024-03-30 18:39:11 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import SelectFlight from "./pages/SelectFlight";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div>
-      <SelectFlight />
+      <LandingPage />
+      {/* <SelectFlight /> */}
     </div>
   );
 }
