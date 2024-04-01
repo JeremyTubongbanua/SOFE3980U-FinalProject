acc66255033bc246998b46afafe56916bc107453
JerryShum <jerryshum2636@gmail.com>
2024-04-01 00:15:50 -0400

    alina branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import SelectFlight from "./pages/SelectFlight";
+import LandingPage from "./pages/LandingPage.js";
 
 function App() {
   return (
     <div>
-      <SelectFlight />
+      <LandingPage />
     </div>
   );
 }
